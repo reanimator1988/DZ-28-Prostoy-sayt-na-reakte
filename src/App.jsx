43652e6bb5ed19043a653d6f77326783f04daea7
DiMa <francev.dima@gmail.com>
2023-08-24 21:31:41 +0200

    dz

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export default () => {
+    return <div>
+        <header>
+            <h1>ДЗ 28. Простий сайт на react</h1>
+            <ul className="nav-menu">
+                <li><a href="#">ГОЛОВНА</a></li>
+                <li><a href="#">ПИТАННЯ</a></li>
+                <li><a href="#">МАТЕРІАЛИ</a></li>
+                <li><a href="#">КОНТАКТИ</a></li>
+            </ul>
+        </header>
+    </div>
+}
+
---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './App.scss';
+import App from './App.jsx'
+
+
+
+const rootE1 = document.getElementById('root');
+const root = ReactDOM.createRoot(rootE1);
+
+root.render(<App />);
+
