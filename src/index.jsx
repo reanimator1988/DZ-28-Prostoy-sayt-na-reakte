e314af9f9e42f5628926d60a1413689f58a4f104
DiMa <francev.dima@gmail.com>
2023-08-30 20:09:28 +0200

    пересдача

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './App.scss';
-import Header from './Header.jsx'
-import Main from './Main.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App.jsx";
+import "./App.scss";
 
-
-
-const rootE1 = document.getElementById('root');
-const root = ReactDOM.createRoot(rootE1);
-
-root.render(
-    <>
-      <Header />
-      <Main />
-    </>
-)
-
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
